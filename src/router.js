23c552d18bfd7de3aaa0868057c96107abe71fec
Raymond Yang <raymond.yang@centrapay.com>
2021-12-08 22:13:59 +1300

    Add contact page, lint missing semicolons

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import Home from './views/Home';
+import Contact from './views/Contact';
 
 Vue.use(VueRouter);
 
@@ -10,11 +11,30 @@ const routes = [
     path: '/',
     component: Home,
   },
+  {
+    path: '/contact',
+    component: Contact,
+  }
 ];
 
 const router = new VueRouter({
   mode: 'history',
   routes
 });
+
+/* Print NavigationDuplicated errors to console as warnings */
+const originalPush = router.push;
+router.push = function push(location, onResolve, onReject) {
+  if (onResolve || onReject) {
+    return originalPush.call(this, location, onResolve, onReject);
+  }
+  return originalPush.call(this, location).catch((err) => {
+    if (VueRouter.isNavigationFailure(err)) {
+      console.warn(err);
+      return err;
+    }
+    return Promise.reject(err);
+  });
+};
 
 export default router;
---
+++ src/views/Contact.vue
@@ -0,0 +1,95 @@
+<template>
+  <v-container fluid>
+    <v-row
+      class="text-h4 d-flex justify-center mb-5"
+      style="text-align:center;"
+    >
+      Interested to get in touch? <br>
+      Here are ways you can contact me!
+    </v-row>
+    <v-row class="d-flex justify-center mb-15">
+      <v-col class="d-flex justify-center">
+        <a
+          id="send-email-button"
+          href="mailto:raymondyang@outlook.co.nz"
+          target="_blank"
+        >
+          <v-icon
+            x-large
+            color="teal"
+          >
+            mdi-email
+          </v-icon>
+        </a>
+      </v-col>
+      <v-col class="d-flex justify-center">
+        <a
+          id="linked-in-button"
+          href="https://www.linkedin.com/in/raymond-yang-96365419a/"
+          target="_blank"
+        >
+          <v-icon
+            x-large
+            color="blue darken-2"
+          >
+            mdi-linkedin
+          </v-icon>
+        </a>
+      </v-col>
+      <v-col class="d-flex justify-center">
+        <a
+          id="github-button"
+          href="https://github.com/Ray2126"
+          target="_blank"
+        >
+          <v-icon
+            x-large
+          >
+            mdi-github
+          </v-icon>
+        </a>
+      </v-col>
+    </v-row>
+    <v-row
+      class="text-h4 d-flex justify-center mb-5"
+      style="text-align:center;"
+    >
+      And here is my CV!
+    </v-row>
+    <v-row class="d-flex justify-center mb-15">
+      <v-btn 
+        class="ma-2" 
+        outlined 
+        href="Raymond_Yang_CV.pdf"
+        target="blank"
+      >
+        View
+      </v-btn>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'LandingPage',
+};
+
+</script>
+
+<style scoped>
+
+#send-email-button:link,
+#linked-in-button:link,
+#github-button:link {
+    text-decoration: inherit;
+    color: inherit;
+}
+
+#send-email-button:visited,
+#linked-in-button:visited,
+#github-button:visited {
+    text-decoration: inherit;
+    color: inherit;
+}
+</style>
